a18f5fed3f4423808d5bca53b073d127a03709b7
Zihan <1723893881@qq.com>
2024-05-23 23:18:38 +0800

    尝试解决跨域http-proxy-middleware.3

--- api/proxy.js
+++ api/proxy.js
@@ -7,7 +7,7 @@ module.exports = (req, res) => {
     // 代理目标地址
     // 这里使用 backend 主要用于区分 vercel serverless 的 api 路径
     // target 替换为你跨域请求的服务器 如： http://baidu.com
-    if (req.url.startsWith('/backend')) {
+    if (req.url.includes('/backend')) {
         target = 'http://codercba.com:9002'
     }
     // 创建代理对象并转发请求
